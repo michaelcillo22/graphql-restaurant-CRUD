4c78c9857b992f34aeabc4057cbec3de1703c2d0
mjc_macbook <62421844+michaelcillo22@users.noreply.github.com>
2022-09-04 13:09:35 -0400

    include package.json files

--- index.js
+++ index.js
@@ -5,59 +5,122 @@ var express = require("express");
 // Construct a schema, using GraphQL schema language
 var restaurants = [ // data
     {
-      id: 1
-      name: "WoodsHill ",
-      description:
+        id: 1
+        name: "WoodsHill ",
+        description:
         "American cuisine, farm to table, with fresh produce every day",
-      dishes: [
+        dishes: [
         {
-          name: "Swordfish grill",
-          price: 27,
+            name: "Swordfish grill",
+            price: 27,
         },
         {
-          name: "Roasted Broccily ",
-          price: 11,
+            name: "Roasted Broccily ",
+            price: 11,
         },
-      ],
+        ],
     },
     {
-      id: 2,
-      name: "Fiorellas",
-      description:
+        id: 2,
+        name: "Fiorellas",
+        description:
         "Italian-American home cooked food with fresh pasta and sauces",
-      dishes: [
+        dishes: [
         {
-          name: "Flatbread",
-          price: 14,
+            name: "Flatbread",
+            price: 14,
         },
         {
-          name: "Carbonara",
-          price: 18,
+            name: "Carbonara",
+            price: 18,
         },
         {
-          name: "Spaghetti",
-          price: 19,
+            name: "Spaghetti",
+            price: 19,
         },
-      ],
+        ],
     },
     {
-      id: 3,
-      name: "Karma",
-      description:
+        id: 3,
+        name: "Karma",
+        description:
         "Malaysian-Chinese-Japanese fusion, with great bar and bartenders",
-      dishes: [
+        dishes: [
         {
-          name: "Dragon Roll",
-          price: 12,
+            name: "Dragon Roll",
+            price: 12,
         },
         {
-          name: "Pancake roll ",
-          price: 11,
+            name: "Pancake roll ",
+            price: 11,
         },
         {
-          name: "Cod cakes",
-          price: 13,
+            name: "Cod cakes",
+            price: 13,
         },
-      ],
+        ],
     },
-  ];
+];
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+type Query{
+  restaurant(id: Int): restaurant
+  restaurants: [restaurant]
+},
+type restaurant {
+  id: Int
+  name: String
+  description: String
+  dishes:[Dish]
+}
+type Dish{
+  name: String
+  price: Int
+}
+input restaurantInput{
+  name: String
+  description: String
+}
+type DeleteResponse{
+  ok: Boolean!
+}
+type Mutation{
+  setrestaurant(input: restaurantInput): restaurant
+  deleterestaurant(id: Int!): DeleteResponse
+  editrestaurant(id: Int!, name: String!): restaurant
+}
+`);
+// The root provides a resolver function for each API endpoint
+var root = {
+    restaurant: (arg) => { // This gets a single restaurant based on a provided ID
+        
+
+
+
+    },
+    restaurants: () => { // This gets a list of all restaurants. 
+        // Your code goes here
+    },
+    setrestaurant: ({ input }) => { // This creates a new restaurant.
+        // Your code goes here
+    },
+    deleterestaurant: ({ id }) => { // This deletes a restaurant based on the provided id.
+        // Your code goes here
+    },
+    editrestaurant: ({ id, ...restaurant }) => { // This updates a restaurant based on the provided id.
+        // Your code goes here
+    },
+    };
+    var app = express();
+    app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
+var port = 5500;
+app.listen(5500, () => console.log("Running Graphql on Port:" + port));
+
+export default root;
